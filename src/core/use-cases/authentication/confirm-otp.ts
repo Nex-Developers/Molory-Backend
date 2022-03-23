d99b5beb75c2930698772b3455e8ccb1059394d4
Kode Sonik <k0d3.s0n1k@gmail.com>
2022-03-23 23:57:20 +0000

    remove email initialisation on confirm otp

--- src/core/use-cases/authentication/confirm-otp.ts
+++ src/core/use-cases/authentication/confirm-otp.ts
@@ -41,7 +41,6 @@ export default function makeConfirmOtp({
                         firstName: "",
                         lastName: "",
                         language: lang,
-                        email: "",
                         password: ""
                     }
                 })
