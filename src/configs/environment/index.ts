e1cf598bc125aaf7267d8ad3e6444b813ae14282
Kode Sonik <k0d3.s0n1k@gmail.com>
2022-07-20 12:33:35 +0000

    fix node env variable

--- src/configs/environment/index.ts
+++ src/configs/environment/index.ts
@@ -1,7 +1,7 @@
 import environment from "./environment"
 import environmentProd from "./environment.prod"
 
-const env = process.env.NOD_ENV=='production'?environmentProd:environment
+const env = process.env.NODE_ENV=='production'?environmentProd:environment
 
 export {
     env
