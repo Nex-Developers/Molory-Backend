3eae3117dadb672ff7973cd6d8af51d4ebba2c3a
Kode Sonik <k0d3.s0n1k@gmail.com>
2022-07-20 12:55:00 +0000

    ask to confirm mail

--- src/initializer.ts
+++ src/initializer.ts
@@ -1,26 +1,33 @@
 import { Role } from "./core/conventions"
+import { askToConfirmEmail } from "./core/services/email"
+import { generateToken } from "./core/services/token"
 import { addUser } from "./core/use-cases/user"
 import { UserDb } from "./db"
 
 export default async () => {
     const userDb = new UserDb()
-    const admin = await userDb.findFirst({ where: { email: 'developer@nex-softwares.com' }, select: { id: true }})
-    if (!admin) {
-        const data = {
-            email: 'developer@nex-softwares.com',
-            password: 'Nex@2022',
-            role: Role.ADMIN,
-            firstName: 'Admin',
-            lastName: 'NEX',
-            phoneNumber: '22890909090',
-            language: 'en',
-            emailVerifiedAt: new Date(),
-            birthDay: new Date()
+    const admin = await userDb.findFirst({ where: { email: 'developer@nex-softwares.com' }, select: { id: true } })
+    if (admin) {
+        if (admin.emailVerifiedAt) {
+            console.log('Admin already exists')
+            return
         }
-        const response = await addUser(data)
-       console.log(`--> Admin user created with id: ${response.id}`)
-    } else {
-        console.log(`--> Admin user already exists`)
+        const token = await generateToken({ email: admin.email})
+         await askToConfirmEmail({ email: admin.email, firstName: admin.firstName, token, lang: admin.language})
+         return
     }
+    const data = {
+        email: 'developer@nex-softwares.com',
+        password: 'Nex@2022',
+        role: Role.ADMIN,
+        firstName: 'Admin',
+        lastName: 'NEX',
+        phoneNumber: '22890909090',
+        language: 'en',
+        emailVerifiedAt: new Date(),
+        birthDay: new Date()
+    }
+    const response = await addUser(data)
+    console.log(`--> Admin user created with id: ${response.id}`)
 
 }
