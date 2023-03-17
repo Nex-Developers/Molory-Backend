805b537e744658da546e3f22f2cc06f783c630ea
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-03-17 14:42:13 +0000

    update dev/prod mode

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,6 @@ try {
         await Mailer.connect()
         await initializer()
         FirebaseAdmin.connect()
-        //send a notification
-        // await FirebaseAdmin.sendNotification(
-        //     [
-        //         "fsNT-iUNQl2RgV_l3dLwxc:APA91bEEHGOerFccgyBL-jpWjwRE7vZFj3VQ84c4YqLKM5gz7Cn3kax9X6ZzrckQz9393N-V0F4a60m-WkbaqJ1SAldSFQsd4kiDyf_Tfhfs2HOFEiMO50cew06F7fZP-zTA4M18ZMPq",
-        //         "cJO-zTFJwkesuCbMPeOToI:APA91bETD3L2cKuAuq-b3AB68TnGV9roMuyj5bQLMc98GnWIPNyiSJm43-B2GtgspXc4rKxSVSc1mkMUAfrTuq8R4mKnWcIiW2kz1-ofTlw8wj8GlcHKxCbAyrv-V-NPCLXiIWLlD-gj"
-        //     ], 
-        //     "Error e9093i.",
-        //      "Les notifications ne marche pas.", 
-        //      null, 
-        //      null)
-        // await SmsServer.send(['22890494008'], 'Test Nemo SMS service API for Molory service')
         app.listen(env.port, () => {
             console.log(`env args production: `, environment.production )
             console.log(`Server  running at http://localhost:${env.port} since `, new Date())
