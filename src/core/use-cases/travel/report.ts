ddc3971ef69d37044c861f8bbe70b7acb4148e02
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-03-26 08:51:06 +0000

    add travel interruption

--- src/core/use-cases/travel/report.ts
+++ src/core/use-cases/travel/report.ts
@@ -6,13 +6,17 @@ export default function makeReport() {
     return async ({
         travelId,
         description,
-        userId 
+        userId,
+        interrupted 
     }: any = {}) => {
         const prisma = DbConnection.prisma
         if (!travelId) throw new MissingParamError('travelId')
         if (!description) throw new MissingParamError('description')
-     
-        await prisma.travelReport.create({  data: {  description, user: { connect: { id: userId }}, travel: { connect: { id: travelId}}  }})
+        
+        await prisma.travelReport.create({  data: {  description, interrupted, user: { connect: { id: userId }}, travel: { connect: { id: travelId}}  }})
+        if (interrupted) {
+            await prisma.travel.update({ where: {id: travelId}, data: { status: -1 }  })
+        }
         const message = { text: "response.add" }
         return { message }
     } 
