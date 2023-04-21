82f85820caef45d5a97ec2c30406fdca9b4db6e2
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-04-21 22:07:01 +0000

    Fix build before deploy

--- dist/core/use-cases/authentication/remove-account.js
+++ dist/core/use-cases/authentication/remove-account.js
@@ -2,13 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const errors_1 = require("../../../utils/errors");
-function makeRemoveAccount({ removeToken, userDb } = {}) {
-    if (!removeToken || !userDb)
+const helpers_1 = require("../../../utils/helpers");
+function makeRemoveAccount({ removeToken } = {}) {
+    if (!removeToken)
         throw new errors_1.ServerError();
     return function removeAccount({ token, id } = {}) {
         return (0, tslib_1.__awaiter)(this, void 0, void 0, function* () {
-            yield removeToken({ token }),
-                yield userDb.deleteOne({ where: { id } });
+            const prisma = helpers_1.DbConnection.prisma;
+            const { email, phoneNumber, firstName, lastName } = yield prisma.user.findUnique({ where: { id } });
+            yield prisma.userArchive.create({ data: { id, email, phoneNumber, firstName, lastName } });
+            yield prisma.user.update({ where: { id }, data: { email: id, phoneNumber: id, firstName: 'Deleted', lastName: 'Account', deletedAt: new Date() } });
+            yield removeToken({ token });
             const message = { text: 'auth.message.removeAccount' };
             return { message };
         });
