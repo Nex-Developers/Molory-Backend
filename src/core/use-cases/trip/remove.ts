71a7301932ddf22028d0ae91e790eb319b0db834
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-08-19 19:48:16 +0000

    Update fees

--- src/core/use-cases/trip/remove.ts
+++ src/core/use-cases/trip/remove.ts
@@ -81,7 +81,7 @@ export default function makeRemove({
                             await prisma.transaction.update({ where: {id: payment.id}, data: { status: 0 }})
                             const transactionId = v4()
                             if (delay < new Date()) {
-                                const sanction = Math.ceil((0.5 * (principal.price + principal.fees))/5) * 5
+                                const sanction = Math.ceil((0.5 * (principal.price))/5) * 5
                                 console.log('sanction ', userId, sanction)
                                 await prisma.wallet.update({ where: { id: userId }, data: { balance: { decrement:  sanction} } })
                                 // Notify the driver
