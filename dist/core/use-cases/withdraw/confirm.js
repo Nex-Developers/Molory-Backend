e08c3808915fe442c09f127bd11978027fc2c5cc
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-08-25 07:55:17 +0000

    Update transactions

--- dist/core/use-cases/withdraw/confirm.js
+++ dist/core/use-cases/withdraw/confirm.js
@@ -13,16 +13,18 @@ function makeConfirm({ updateTransaction, saveProfile } = {}) {
             throw new errors_1.MissingParamError('body');
         const prisma = helpers_1.DbConnection.prisma;
         const { entity, name } = body;
-        console.log(name);
+        console.log(name, entity.status);
         if (!entity && !entity.id)
             return { recieved: false };
-        console.log('status', entity.status);
         const status = entity.status === 'sent' ? 1 : 0;
         const transaction = yield prisma.transaction.findUnique({ where: { id: 'trans-' + entity.id } });
         if (transaction.status !== 2)
             throw new errors_1.AlreadyDoneError(transaction.createdAt.toString());
-        yield prisma.wallet.update({ where: { id: transaction.walletId }, data: { balance: { decrement: transaction.amount } } });
-        yield prisma.transaction.update({ where: { id: transaction.id }, data: { status, method: entity.mode } });
+        if (status === 1) {
+            console.log('withraw', transaction.walletId, transaction.amount);
+            yield prisma.wallet.update({ where: { id: transaction.walletId }, data: { balance: { increment: -transaction.amount } } });
+        }
+        yield prisma.transaction.update({ where: { id: transaction.id }, data: { status, method: entity.mode, validatedAt: new Date() } });
         yield updateTransaction(entity.id, status, { method: entity.mode });
         yield saveProfile(transaction.walletId);
         return { recieved: true };
--- src/core/use-cases/withdraw/confirm.ts
+++ src/core/use-cases/withdraw/confirm.ts
@@ -7,23 +7,23 @@ export default function makeConfirm({
 }: any = {}) {
     if (!updateTransaction || !saveProfile) throw new ServerError()
     return async ({
-        token,
         body
     }: any = {}) => {
-        if (!token) throw new MissingParamError('token')
         if (!body) throw new MissingParamError('body')
         const prisma = DbConnection.prisma
         const { entity, name } = body
-        console.log(name)
+        console.log(name, entity.status)
         if (!entity && !entity.id) return { recieved: false }
-        console.log('status', entity.status)
         const status = entity.status === 'sent' ? 1 : 0
 
         // Update the satus
         const transaction = await prisma.transaction.findUnique({ where: { id: 'trans-'+ entity.id } })
         if (transaction.status !== 2) throw new AlreadyDoneError(transaction.createdAt.toString())
-        await prisma.wallet.update({ where: { id: transaction.walletId }, data: { balance: { decrement: transaction.amount } } })
-        await prisma.transaction.update({ where: { id: transaction.id }, data: { status, method: entity.mode } })
+        if (status === 1) {
+            console.log('withraw', transaction.walletId, transaction.amount)
+            await prisma.wallet.update({ where: { id: transaction.walletId }, data: { balance: { increment: -transaction.amount } } })
+        }
+        await prisma.transaction.update({ where: { id: transaction.id }, data: { status, method: entity.mode, validatedAt: new Date() } })
         await updateTransaction(entity.id, status, { method: entity.mode})
         await saveProfile(transaction.walletId)
 
