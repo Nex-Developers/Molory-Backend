cc128f909e3016539f14c10c6446518a22e49e7b
Kode Sonik <k0d3.s0n1k@gmail.com>
2023-12-13 11:57:43 +0000

    Add send slack message after document submission

--- dist/utils/helpers/slack-messaging.js
+++ dist/utils/helpers/slack-messaging.js
@@ -1,20 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const { WebClient } = require('@slack/web-api');
+const bolt_1 = (0, tslib_1.__importDefault)(require("@slack/bolt"));
 class SlackMessaging {
     static sendMessage(message, channel) {
         return (0, tslib_1.__awaiter)(this, void 0, void 0, function* () {
-            const web = new WebClient('xoxb-6323062419703-6340703978260-4VpQLD1RTKVUe6yOoDRjzaF3', {});
             return new Promise((resolve, reject) => {
                 const channelId = channel || process.env.SLACK_CHANNEL_ID;
                 try {
-                    return web.conversations.join({
-                        channel: channel,
-                    }).then(() => web.chat.postMessage({
+                    return SlackMessaging.app.client.chat.postMessage({
+                        token: process.env.SLACK_BOT_TOKEN || "xoxb-6323062419703-6340703978260-4LLTYsWIl331T19u5PtqalvY",
+                        text: message,
                         blocks: message,
                         channel: channelId,
-                    }).then(() => resolve(true)));
+                    }).then(() => resolve(true));
                 }
                 catch (error) {
                     return resolve(true);
@@ -24,4 +23,8 @@ class SlackMessaging {
     }
 }
 exports.default = SlackMessaging;
+SlackMessaging.app = new bolt_1.default.App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET || "6eb02e580965aa5df77b528c83eea846",
+    token: process.env.SLACK_BOT_TOKEN || "xoxb-6323062419703-6340703978260-4LLTYsWIl331T19u5PtqalvY",
+});
 //# sourceMappingURL=slack-messaging.js.map
--- src/utils/helpers/slack-messaging.ts
+++ src/utils/helpers/slack-messaging.ts
@@ -1,20 +1,25 @@
-const { WebClient } = require('@slack/web-api');
+// const { WebClient } = require('@slack/web-api');
+import Slack from "@slack/bolt";
 
 
 export default class SlackMessaging {
     // private static options = {};
-
+    private static app = new Slack.App({
+        signingSecret: process.env.SLACK_SIGNING_SECRET || "6eb02e580965aa5df77b528c83eea846",
+        token: process.env.SLACK_BOT_TOKEN || "xoxb-6323062419703-6340703978260-4LLTYsWIl331T19u5PtqalvY",
+      });
+      
     static async sendMessage(message, channel) {
-        const web = new WebClient('xoxb-6323062419703-6340703978260-4VpQLD1RTKVUe6yOoDRjzaF3', {});
+        // const web = new WebClient('xoxb-6323062419703-6340703978260-4LLTYsWIl331T19u5PtqalvY', {});
         return new Promise((resolve, reject) => {
             const channelId = channel || process.env.SLACK_CHANNEL_ID;
             try {
-                return web.conversations.join({
-                    channel: channel,
-                }).then(() => web.chat.postMessage({
+                return  SlackMessaging.app.client.chat.postMessage({
+                    token: process.env.SLACK_BOT_TOKEN || "xoxb-6323062419703-6340703978260-4LLTYsWIl331T19u5PtqalvY",
+                    text: message,
                     blocks: message,
                     channel: channelId,
-                }).then(() => resolve(true)));
+                }).then(() => resolve(true));
 
             } catch (error) {
                 return resolve(true);
